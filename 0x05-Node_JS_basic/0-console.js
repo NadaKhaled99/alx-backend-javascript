1950949da1ad3cfb03986339c17875369bd16796
Nada <nadafarrag619@gmail.com>
2024-08-21 00:13:33 +0300

    file

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Logs a message to the console
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function
+ * @module displayMessage
+ */
+module.exports = displayMessage;
